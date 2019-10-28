dd8cff14a0403a0ca04a2519dea0c41f9bc4218c
elchangtw <elchang.tw@gmail.com>
2019-10-28 12:34:28 -0700

    added remaining classes

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,18 @@
 var ForagerBee = function() {
+  Bee.call(this);
+
+  this.age = 10;
+  this.job = 'find pollen';
+  //a color property inherited from bee that is set to yellow
+  //a food property that is inherited from grub
+  //an eat method that is inherited from grub
+  this.canFly = true;
+  this.treasureChest = [];
 };
 
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+
+ForagerBee.prototype.forage = function(treasure) {
+  this.treasureChest.push(treasure);
+}
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,21 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+
+  this.age = 10;
+  this.job = 'make honey';
+  //a color property inherited from bee that is set to yellow
+  //a food property that is inherited from grub
+  //an eat method that is inherited from grub
+  this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+}
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+}
